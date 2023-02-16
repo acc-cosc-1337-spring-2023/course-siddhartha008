bc48a2fe93c16828daa091d4c76e33feee0b47f9
Sid <83157044+siddhartha008@users.noreply.github.com>
2023-02-16 16:49:01 +0000

    homework3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,60 @@
-//write include statement for decisions header
+// write include statement for decisions header
+#include "decisions.h"
+using std::string;
 
+// Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    if (grade >= 90 && grade <= 100)
+    {
+        return "A";
+    }
+    else if (grade >= 80 && grade < 90)
+    {
+        return "B";
+    }
+    else if (grade >= 70 && grade < 80)
+    {
+        return "C";
+    }
+    else if (grade >= 60 && grade < 70)
+    {
+        return "D";
+    }
+    else
+    {
+        return "F";
+    }
+}
 
-//Write code for function(s) code here
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string letter_grade;
+    switch (grade / 10) // dividing by 10 to get a number between 0-10
+    {
+    //(90-100)
+    case 10:
+    case 9:
+        letter_grade = "A";
+        break;
+    //(80-90)
+    case 8:
+        letter_grade = "B";
+        break;
+    //(70-80)
+    case 7:
+        letter_grade = "C";
+        break;
+
+    //(60-70)
+    case 6:
+        letter_grade = "D";
+        break;
+
+    //(0-50)
+    default: // this is like the else in an if statement
+        letter_grade = "F";
+        break;
+    }
+    return letter_grade;
+}
--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -1,7 +1,31 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification")
+{
 	REQUIRE(true == true);
 }
+
+TEST_CASE("Verify Grade Conversion using if_else_if")
+{
+	REQUIRE(get_letter_grade_using_if(95) == "A");
+	REQUIRE(get_letter_grade_using_if(85) == "B");
+	REQUIRE(get_letter_grade_using_if(75) == "C");
+	REQUIRE(get_letter_grade_using_if(65) == "D");
+	REQUIRE(get_letter_grade_using_if(0) == "F");
+
+}
+
+
+TEST_CASE("Verify Grade Conversion using switch statements")
+{
+	REQUIRE(get_letter_grade_using_if(95) == "A");
+	REQUIRE(get_letter_grade_using_if(85) == "B");
+	REQUIRE(get_letter_grade_using_if(75) == "C");
+	REQUIRE(get_letter_grade_using_if(65) == "D");
+	REQUIRE(get_letter_grade_using_if(0) == "F");
+
+}
+
 
